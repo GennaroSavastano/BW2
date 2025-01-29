525e34e11d8af346b864f496461df90d8699912d
SilviaFranzinelli <franzinellisilvia@gmail.com>
2025-01-29 21:45:30 +0100

    fetch artist che parto

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -1,8 +1,9 @@
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 const artista = [];
-const URL = `https://striveschool-api.herokuapp.com/api/deezer/artist/${id}`;
-fetch(URL)
+const URL = `https://striveschool-api.herokuapp.com/api/deezer/`;
+let folder = `artist/${id}`;
+fetch(URL + folder)
   .then((resp) => {
     if (resp.ok) {
       return resp.json();
@@ -18,7 +19,51 @@ fetch(URL)
 
 function imgs(artista) {
   const imgArt = document.getElementById("artista");
-  console.log(artista.picture);
+  console.log(artista[0].picture);
   imgArt.style.backgroundImage = "url(" + artista[0].picture + ")";
   imgArt.style.backgroundColor = "red";
+  imgArt.style.backgroundRepeat = "no-repeat";
+  imgArt.style.backgroundSize = "cover";
+  imgArt.style.backgroundPosition = "center";
+  return nameArtist(artista);
+}
+
+function nameArtist(artista) {
+  const name = document.querySelector("h1");
+  name.innerText = artista[0].name;
+  const fans = document.getElementById("fans");
+  fans.innerText = parseInt(artista[0].nb_fan).toLocaleString("it-IT");
+}
+
+folder += `/top?limit=5`;
+// const popular = [];
+fetch(URL + folder)
+  .then((resp) => {
+    if (resp.ok) {
+      return resp.json();
+    } else {
+      throw new Error("Ci dispiace non siamo riusciti a recuperare l'artista");
+    }
+  })
+  .then((topList) => {
+    const popular = [...topList.data];
+    console.log(popular);
+    listPopular(popular);
+  });
+
+function listPopular(popular) {
+  const containerPopular = document.getElementById("containerPopular");
+  console.log(popular.length);
+  // titolo1.innerText = popular[0][0].title;
+  for (let i = 0; i < popular.length; i++) {
+    containerPopular.innerHTML += `<div class="row m-1">
+                                    <div class="col-4 d-flex">
+                                      <span class="align-self-center pe-1">1</span>
+                                      <img src="${popular[i].album.cover}" width="45px" class="mb-2 mx-2 rounded" />
+                                      <span>${popular[i].title}</span>
+                                    </div>
+                                    <div class="col-4 text-end">30.964.869</div>
+                                    <div class="col-4 text-end"><span>3:09</span></div>
+                                  </div>`;
+  }
 }
