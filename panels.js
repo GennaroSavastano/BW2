53a37d266cd02786dcef3f665fce759ac7a67ca6
“Juliet” <julietasiamah234@gmail.com>
2025-01-29 11:37:39 +0100

    sidebar2

--- panels.js
+++
@@ -1,8 +0,0 @@
-const shortArrow = document.querySelector(".short-arrow");
-const leftSidebar = document.querySelector("leftsidebar");
-
-function arrowRight(event) {
-  console.log(event);
-}
-
-//shortArrow.addEventListener("click", callback);
