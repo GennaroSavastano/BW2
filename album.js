4f4667cb4ac762b3074ae77dbde03ec7b2b70fb7
GennaroSavastano <tantormainondormopiu@gmail.com>
2025-01-30 12:50:34 +0100

    2nd

--- album.js
+++ album.js
@@ -20,6 +20,7 @@ fetch(URL, {
   .then((album) => {
     console.log(album);
     createAlbumInfo(album);
+    createTrackList(album);
   });
 
 const albumInfo = document.getElementById("albumInfo");
@@ -45,4 +46,61 @@ function formatTime(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
   return `${minutes} min ${remainingSeconds.toString().padStart(2, "0")} sec.`;
+}
+
+function createTrackList(album) {
+  const trackList = document.getElementById("trackList");
+
+  album.tracks.data.forEach((track, index) => {
+    const trackItem = document.createElement("div");
+    trackItem.classList.add = ("row", "d-flex", "justify-content-between");
+    trackItem.innerHTML = `<div class="col-auto d-flex flex-column align-items-center">
+                    <p class="ms-3">${[index] + 1}</p>
+                    <button class="border-0 bg-transparent fs-2 d-none" type="button">
+                      <i class="bi bi-play-fill"></i>
+                    </button>
+                  </div>
+                  <div class="col-8 d-flex align-items-center justify-content-between">
+                    <div>
+                      <a href="${track.link}">${track.title}</a>
+                      <a href="${track.artist.name}">${track.artist.name}</a>
+                    </div>
+                    <div>
+                      <button type="button" class="btn ms-3 d-none">
+                        <svg data-encore-id="icon" role="img" aria-hidden="true" class="svgicon" viewBox="0 0 24 24">
+                          <path
+                            d="M11.999 3a9 9 0 1 0 0 18 9 9 0 0 0 0-18zm-11 9c0-6.075 4.925-11 11-11s11 4.925 11 11-4.925 11-11 11-11-4.925-11-11z"
+                          ></path>
+                          <path
+                            d="M17.999 12a1 1 0 0 1-1 1h-4v4a1 1 0 1 1-2 0v-4h-4a1 1 0 1 1 0-2h4V7a1 1 0 1 1 2 0v4h4a1 1 0 0 1 1 1z"
+                          ></path>
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                  <div class="col-2 d-flex align-items-center">
+                    <p>${formatTime2(track.duration)}</p>
+                    <div class="dropdown ms-3">
+                      <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg data-encore-id="icon" role="img" aria-hidden="true" class="svgicon" viewBox="0 0 24 24">
+                          <path
+                            d="M4.5 13.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm15 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm-7.5 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"
+                          ></path>
+                        </svg>
+                      </button>
+                      <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                      </ul>
+                    </div>
+                  </div>`;
+    trackList.appendChild(trackItem);
+  });
+}
+
+function formatTime2(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
 }
