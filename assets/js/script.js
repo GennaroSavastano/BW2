2c9dfd0ac60cc4e3f648634e407ce818d9a01bd9
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2025-01-30 00:16:15 +0000

    carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -335,3 +335,41 @@ function createRecommended(recommendedSongs) {
   h2.innerText = "Consigliata per oggi";
   //return fetchRecommended(artists, myApiUrl, API_KEY, 0);
 }
+
+// codice per la gestione del carosello
+
+$(".carousel").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
